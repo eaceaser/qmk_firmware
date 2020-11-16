5c378a738c55f93adbd65494c0a6bf83ac308987
Ed Ceaser <ceasere@gmail.com>
2020-11-16 13:50:18 -0800

    disable mouse, more keymaps

--- keyboards/handwired/dactyl_manuform/eac_4x6/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/eac_4x6/keymaps/default/keymap.c
@@ -28,24 +28,6 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/* Base (qwerty)
- * +-----------------------------------------+                             +-----------------------------------------+
- * | TAB  |   q  |   w  |   e  |   r  |   t  |                             |   y  |   u  |   i  |   o  |   p  |  \   |
- * |------+------+------+------+------+------|                             |------+------+------+------+------+------|
- * | ESC  |   a  |   s  |   d  |   f  |   g  |                             |   h  |   j  |   k  |   l  |   ;  |  '   |
- * |------+------+------+------+------+------|                             |------+------+------+------+------+------|
- * | SHFT |   z  |   x  |   c  |   v  |   b  |                             |   n  |   m  |   ,  |   .  |   /  |  -   |
- * +------+------+------+------+-------------+                             +-------------+------+------+------+------+
- * |  `   |   =  |      |      |                                                         |      |      |  [   |  ]   |
- * +-------------+-------------+-------------+                             +-------------+-------------+-------------+
- *                             | LCTRL|  LALT|                             |  SPC | ENT  |
- *                      |------+------+------|                             |------+------|------|
- *                      | RAISE|      |                                           |      |      |
- *                      +-------------+                                           +-------------+
- *                      | HOME | END  |                                           |      |      |
- *                      |------+------|                                           |------+------|
- */
-
 [_BASE] = LAYOUT( \
     KC_TAB,   KC_Q,     KC_W,   KC_E,   KC_R,   KC_T,               KC_Y,   KC_U,   KC_I,    KC_O,   KC_P,    KC_BSLASH, \
     KC_ESC,   KC_A,     KC_S,   KC_D,   KC_F,   KC_G,               KC_H,   KC_J,   KC_K,    KC_L,   KC_SCLN, KC_QUOT,  \
@@ -58,9 +40,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 [_LOWER] = LAYOUT(
-    _______,_______,_______,_______,_______,KC_LBRC,            KC_RBRC, KC_P7, KC_P8,  KC_P9,  RESET,  KC_PLUS,   \
-    _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,            KC_RPRN, KC_P4, KC_P5,  KC_P6,  KC_MINS,KC_PIPE,   \
-    _______,_______,_______,_______,_______,_______,            _______, KC_P1, KC_P2,  KC_P3,  KC_EQL, KC_UNDS,   \
+    _______,_______,_______,_______,_______,KC_LBRC,            KC_RBRC, KC_P7, KC_P8,  KC_P9,  RESET,  KC_MUTE,   \
+    _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,            KC_RPRN, KC_P4, KC_P5,  KC_P6,  KC_MINS,KC_VOLU,   \
+    _______,_______,_______,_______,_______,_______,            _______, KC_P1, KC_P2,  KC_P3,  KC_EQL, KC_VOLD,   \
     _______,_______,_______,_______,                                            _______,_______,_______,_______,   \
                                             _______,HYPER,      _______, KC_P0,                                    \
                                             _______,_______,    _______,_______,                                   \
@@ -68,8 +50,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_RAISE] = LAYOUT(
-    _______,KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,            KC_6   ,KC_7   ,KC_8   ,KC_9   , KC_0  ,KC_MUTE,   \
-    _______,KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,KC_LPRN,            KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT,_______,KC_VOLU,   \
+    _______,KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,            KC_6   ,KC_7   ,KC_8   ,KC_9   , KC_0  ,KC_EQUAL,   \
+    _______,_______,_______,_______,_______,_______,            KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT,_______,KC_VOLU,   \
     _______,_______,_______,_______,_______,_______,            _______,_______,_______,_______,_______,KC_VOLD,   \
     _______,_______,_______,_______,                                            _______,_______,_______,_______,   \
                                             HYPER  ,_______,    KC_DEL, _______,                                   \
@@ -79,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_HYPER] = LAYOUT(
     _______,KC_F1  ,KC_F2   ,KC_F3  ,KC_F4   ,KC_F5,            KC_F6  ,KC_F7  ,KC_F8  ,KC_F9  , KC_F10,KC_MUTE,   \
-    _______,KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,KC_LPRN,            KC_RPRN,KC_MPRV,KC_MPLY,KC_MNXT,_______,KC_VOLU,   \
+    _______,_______,_______,_______,_______,_______,            KC_RPRN,KC_MPRV,KC_MPLY,KC_MNXT,_______,KC_VOLU,   \
     _______,_______,_______,_______,_______,_______,            _______,_______,_______,_______,_______,KC_VOLD,   \
     _______,_______,_______,_______,                                            _______,_______,_______,_______,   \
                                             _______,_______,    KC_DEL, _______,                                   \
