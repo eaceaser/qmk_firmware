fc2dc9053a5d6b820c2fe72fcf71f61d6f4bae34
Ed Ceaser <ceasere@gmail.com>
2020-11-23 10:54:20 -0800

    backlighting

--- keyboards/handwired/dactyl_manuform/eac_4x6/config.h
+++ keyboards/handwired/dactyl_manuform/eac_4x6/config.h
@@ -40,3 +40,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // WS2812 RGB LED strip input and number of LEDs
 // #define RGB_DI_PIN D3
 // #define RGBLED_NUM 12
+
+/* rgbs */
+#define RGB_DI_PIN F0
+#define RGBLED_NUM 14
+#define RGBLIGHT_SPLIT
+#define RGBLED_SPLIT {7, 7}
+//#define RGBLIGHT_MODE_BREATHING 1
--- keyboards/handwired/dactyl_manuform/eac_4x6/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/eac_4x6/keymaps/default/keymap.c
@@ -1,6 +1,5 @@
 #include QMK_KEYBOARD_H
 
-
 #define _BASE 0
 #define _RAISE 1
 #define _LOWER 2
@@ -32,30 +31,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,     KC_W,   KC_E,   KC_R,   KC_T,               KC_Y,   KC_U,   KC_I,    KC_O,   KC_P,    KC_BSLASH, \
     KC_ESC,   KC_A,     KC_S,   KC_D,   KC_F,   KC_G,               KC_H,   KC_J,   KC_K,    KC_L,   KC_SCLN, KC_QUOT,  \
     KC_LSFT,  KC_Z,     KC_X,   KC_C,   KC_V,   KC_B,               KC_N,   KC_M,   KC_COMM, KC_DOT, KC_SLSH, KC_MINUS, \
-    KC_GRAVE, KC_EQUAL, KC_NO,  KC_NO,                                              KC_NO,   KC_NO,  KC_LBRC, KC_RBRC, \
-                                   LOWER, RAISE     ,             KC_SPC, KC_ENT,                                  \
+    KC_GRAVE, KC_EQUAL, KC_HOME,KC_END,                                             KC_PGUP, KC_PGDN,KC_LBRC, KC_RBRC, \
+                                   LOWER, RAISE     ,             KC_ENT, KC_SPC,                                  \
                                    KC_LCTRL, KC_LALT,             KC_BSPC, KC_PGUP,                               \
-                                     KC_LGUI, KC_NO,             KC_PGUP, KC_PGDN                                 \
+                                     KC_LGUI, KC_NO,              KC_PGUP, KC_PGDN                                 \
 ),
 
 
 [_LOWER] = LAYOUT(
-    _______,_______,_______,_______,_______,KC_LBRC,            KC_RBRC, KC_P7, KC_P8,  KC_P9,  RESET,  KC_MUTE,   \
+    _______,_______,_______,_______,_______,_______,            _______, KC_P7, KC_P8,  KC_P9,  RESET,  KC_MUTE,   \
     _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,            KC_RPRN, KC_P4, KC_P5,  KC_P6,  KC_MINS,KC_VOLU,   \
     _______,_______,_______,_______,_______,_______,            _______, KC_P1, KC_P2,  KC_P3,  KC_EQL, KC_VOLD,   \
-    _______,_______,_______,_______,                                            _______,_______,_______,_______,   \
+    _______,_______,RGB_VAI,RGB_TOG,                                            _______,_______,_______,_______,   \
                                             _______,HYPER,      _______, KC_P0,                                    \
                                             _______,_______,    _______,_______,                                   \
                                             _______,_______,    _______,_______                                    \
 ),
 
 [_RAISE] = LAYOUT(
-    _______,KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,            KC_6   ,KC_7   ,KC_8   ,KC_9   , KC_0  ,KC_EQUAL,   \
-    _______,_______,_______,_______,_______,_______,            KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT,_______,KC_VOLU,   \
-    _______,_______,_______,_______,_______,_______,            _______,_______,_______,_______,_______,KC_VOLD,   \
-    _______,_______,_______,_______,                                            _______,_______,_______,_______,   \
-                                            HYPER  ,_______,    KC_DEL, _______,                                   \
-                                            _______,_______,    KC_DEL, _______,                                   \
+    KC_GRAVE ,KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,            KC_6   ,KC_7   ,KC_8   ,KC_9   , KC_0  ,KC_EQUAL,   \
+    _______  ,_______,_______,_______,_______,_______,            KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT,_______,KC_VOLU,   \
+    _______  ,_______,_______,_______,_______,_______,            _______,_______,_______,_______,_______,KC_VOLD,   \
+    _______  ,_______,_______,_______,                                            _______,_______,_______,_______,   \
+                                            HYPER  ,_______,    _______, _______,                                   \
+                                            _______,_______,    KC_DEL , _______,                                   \
                                             _______,_______,    KC_HOME,KC_END                                     \
 ),
 
@@ -63,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,KC_F1  ,KC_F2   ,KC_F3  ,KC_F4   ,KC_F5,            KC_F6  ,KC_F7  ,KC_F8  ,KC_F9  , KC_F10,KC_MUTE,   \
     _______,_______,_______,_______,_______,_______,            KC_RPRN,KC_MPRV,KC_MPLY,KC_MNXT,_______,KC_VOLU,   \
     _______,_______,_______,_______,_______,_______,            _______,_______,_______,_______,_______,KC_VOLD,   \
-    _______,_______,_______,_______,                                            _______,_______,_______,_______,   \
+    _______,_______,_______,_______,                                            _______,_______,_______,EEP_RST,   \
                                             _______,_______,    KC_DEL, _______,                                   \
                                             _______,_______,    _______,_______,                                   \
                                             _______,_______,    _______,_______                                    \
@@ -74,4 +73,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 void persistent_default_layer_set(uint16_t default_layer) {
   eeconfig_update_default_layer(default_layer);
   default_layer_set(default_layer);
+}
+
+void keyboard_post_init_user(void) {
+  rgblight_sethsv_noeeprom(226, 143, 143);
 }
