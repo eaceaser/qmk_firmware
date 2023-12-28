c1570410ea26a1eb04357773e78cb6e6e21c4cdb
Edward Ceaser <ceasere@gmail.com>
2023-12-28 15:20:07 -0800

    eac changes to make splinky work

--- keyboards/bastardkb/charybdis/3x6/v2/splinky_3/config.h
+++ keyboards/bastardkb/charybdis/3x6/v2/splinky_3/config.h
@@ -18,12 +18,14 @@
 #pragma once
 
 /* Handedness. */
-#define MASTER_RIGHT
+#ifdef MASTER_RIGHT
+#undef MASTER_RIGHT
+#endif
 
 // To use the handedness pin, resistors need to be installed on the adapter PCB.
 // If so, uncomment the following code, and undefine MASTER_RIGHT above.
-// #define SPLIT_HAND_PIN GP15
-// #define SPLIT_HAND_PIN_LOW_IS_LEFT  // High -> right, Low -> left.
+#define SPLIT_HAND_PIN GP15
+#define SPLIT_HAND_PIN_LOW_IS_LEFT  // High -> right, Low -> left.
 
 /* VBUS detection. */
 #define USB_VBUS_PIN GP19
